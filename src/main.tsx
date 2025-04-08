fb5a5630bb4d00d1db2c2eb2684d1597b77f35d9
santtiojeda8 <ojedasi2004@gmail.com>
2025-04-07 22:09:35 -0300

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <>
-   <BrowserRouter>
-    <App />
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-    </>
-  
-)
+  </>
+);
